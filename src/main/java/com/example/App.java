faa0e2ef1fcaa4fe4b4d0319a5b69cd71f67e200
Giorgio Daniele Luppina <g.luppina@reply.it>
2025-06-19 11:18:06 +0200

    Introduzione di alcune criticità

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,13 +1,14 @@
 package com.example;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+
+        // Bug: possibile NullPointerException
+        String name = null;
+        System.out.println(name.toLowerCase());
+
+        // Duplicazione: due stampe identiche
+        System.out.println("Duplicated line");
+        System.out.println("Duplicated line");
     }
 }
